d57daf26ac2cb6f603e7f8e28084e68a6621ad32
Logan Kearsley <chronosurfer@gmail.com>
2021-03-04 13:36:51 -0700

    Clean up

--- test/compile-test.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-//import { expect } from 'chai';
-import { compile } from '../src/compile';
-
-describe("Run Tests", () => {
-  it('should parse the module', async() => {
-    console.log(await compile('./wasm.wat'));
-  });
-});
